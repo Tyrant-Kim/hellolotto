c3a247e0b7e4a9945c6a9e064a98ff8683d30466
Tyrant-Kim <34240705+Tyrant-Kim@users.noreply.github.com>
2018-12-31 00:52:24 +0900

    main 변경
    main 변경

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import hellolotto
 
 n_from = 1
-dir_download = 'C:\\Users\\kimmo\\PycharmProjects\\hellolotto'
+dir_download = 'C:\\LocalData'
 hellolotto.download_excel_lotto645(n_from, dir_download)
