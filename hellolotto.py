625ec57e2d1cd610349067db1336edad984f1fa4
Tyrant-Kim <34240705+Tyrant-Kim@users.noreply.github.com>
2018-12-31 00:51:16 +0900

    download경로 추가
    download경로 추가함.

--- hellolotto.py
+++ hellolotto.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-def download_excel_lotto645(nfrom):
+
+def download_excel_lotto645(n_from, dir_download):
     webpage = 'https://www.nlotto.co.kr/gameResult.do?method=byWin'
-    driver = webdriver.Chrome()
+    prefs = {'download.default_directory' : dir_download}
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', prefs)
+    if isinstance(n_from, int):
+        nfrom = str(n_from)
+    else:
+        return False
+
+    driver = webdriver.Chrome(chrome_options=options)
+    # driver = webdriver.Chrome()
     driver.get(webpage)
     select_start = Select(driver.find_element_by_id('drwNoStart'))
     select_start.select_by_value(nfrom)
     driver.find_element_by_id('exelBtn').click()
-
---
+++ main.py
@@ -0,0 +1,5 @@
+import hellolotto
+
+n_from = 1
+dir_download = 'C:\\Users\\kimmo\\PycharmProjects\\hellolotto'
+hellolotto.download_excel_lotto645(n_from, dir_download)
